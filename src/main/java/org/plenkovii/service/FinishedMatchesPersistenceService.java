dadcbcea8a53f58021a6f093111eae38d053ad98
Ilya <ilplenkov@mail.ru>
2025-01-08 00:01:16 +0300

    feat: add saveFinishMatch method

--- src/main/java/org/plenkovii/service/FinishedMatchesPersistenceService.java
+++ src/main/java/org/plenkovii/service/FinishedMatchesPersistenceService.java
@@ -1,4 +1,24 @@
 package org.plenkovii.service;
 
-public class ___FinishedMatchesPersistenceService {
+import org.plenkovii.dao.MatchDao;
+import org.plenkovii.dto.MatchAppDto;
+import org.plenkovii.entity.Match;
+import org.plenkovii.entity.Player;
+
+public class FinishedMatchesPersistenceService {
+    MatchDao matchDao;
+
+    public FinishedMatchesPersistenceService(MatchDao matchDao) {
+        this.matchDao = matchDao;
+    }
+
+    public void saveFinishedMatch(MatchAppDto matchAppDto, String  winnerId) {
+        Player player1 = matchAppDto.getPlayer1();
+        Player player2 = matchAppDto.getPlayer2();
+        Player matchWinner = winnerId.equals("1") ? player1 : player2;
+
+        Match match = new Match(player1, player2, matchWinner);
+
+        matchDao.save(match);
+    }
 }
