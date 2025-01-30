766aacf2dc4c99289d8327f9dc845106778d2890
Ilya <ilplenkov@mail.ru>
2025-01-30 20:09:43 +0300

    feat: add methods for pagination

--- src/main/java/org/plenkovii/dao/MatchDao.java
+++ src/main/java/org/plenkovii/dao/MatchDao.java
@@ -5,5 +5,10 @@ import org.plenkovii.entity.Match;
 import java.util.List;
 
 public interface MatchDao extends CrudDao<Match> {
+
     List<Match> findByPlayerName(String name);
+
+    List<Match> findAllWithPagination(int page, int pageSize);
+
+    List<Match> findByPlayerNameWithPagination(String name, int page, int pageSize);
 }
