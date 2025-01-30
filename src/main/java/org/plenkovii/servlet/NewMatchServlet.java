97254a439484ed9285a350030c9f9f3636457fd2
Ilya <ilplenkov@mail.ru>
2025-01-30 21:25:03 +0300

    fix: delete dao methods

--- src/main/java/org/plenkovii/servlet/NewMatchServlet.java
+++ src/main/java/org/plenkovii/servlet/NewMatchServlet.java
@@ -4,12 +4,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.plenkovii.dao.HibernatePlayerDao;
-import org.plenkovii.dao.PlayerDao;
-import org.plenkovii.dto.PlayerRequestDto;
-import org.plenkovii.entity.Player;
 import org.plenkovii.service.OngoingMatchesService;
-import org.plenkovii.service.SavePlayersService;
 import org.plenkovii.validation.PlayerNameValidation;
 
 import java.io.IOException;
@@ -25,17 +20,9 @@ public class NewMatchServlet extends HttpServlet {
 
         PlayerNameValidation.validate(player1Name, player2Name);
 
-        PlayerRequestDto player1Dto = new PlayerRequestDto(player1Name);
-        PlayerRequestDto player2Dto = new PlayerRequestDto(player2Name);
-
-        PlayerDao playerDao = new HibernatePlayerDao();
-        SavePlayersService savePlayersService = new SavePlayersService(playerDao);
-
-        Player player1 = savePlayersService.savePlayerIfNotPresent(player1Dto);
-        Player player2 = savePlayersService.savePlayerIfNotPresent(player2Dto);
-
         OngoingMatchesService ongoingMatchesService = (OngoingMatchesService) getServletContext().getAttribute("ongoingMatchesService");
-        UUID uuid = ongoingMatchesService.saveNewMatch(player1, player2);
+
+        UUID uuid = ongoingMatchesService.saveNewMatch(player1Name, player2Name);
 
         resp.sendRedirect(req.getContextPath() + "/match-score?uuid=" + uuid.toString());
     }
